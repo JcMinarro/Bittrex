0122a5df7138728f4d78965e359fc4bbe85c87ec
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-12-23 14:36:39 +0100

    Add public Models

---
+++ src/main/kotlin/com/jcminarro/bittrex/Models.kt
@@ -0,0 +1,63 @@
+package com.jcminarro.bittrex
+
+import java.util.*
+
+typealias MarketHistory = List<Trade>
+
+data class Market(val marketCurrencySymbol: String,
+                  val marketCurrencyName: String,
+                  val baseCurrencySymbol: String,
+                  val baseCurrencyName: String,
+                  val name: String,
+                  val minTradeSize: Double,
+                  val active: Boolean,
+                  val created: Date)
+
+data class Currency(val name: String,
+                    val symbol: String,
+                    val minConfirmation: Int,
+                    val txFee: Double,
+                    val active: Boolean,
+                    val coinType: String)
+
+data class Tick(val bid: Double,
+                val ask: Double,
+                val last: Double)
+
+data class MarketSummary(val name: String,
+                         val high: Double,
+                         val low: Double,
+                         val volume: Double,
+                         val last: Double,
+                         val baseVolume: Double,
+                         val bid: Double,
+                         val ask: Double,
+                         val prevDay: Double,
+                         val openBuyOrders: Int,
+                         val openSellOrders: Int,
+                         val timestamp: Date,
+                         val created: Date)
+
+data class Order(val quantity: Double,
+                 val rate: Double)
+
+data class Orderbook(val buy: List<Order>,
+                     val sell: List<Order>)
+
+data class Trade(val id: Int,
+                 val timestamp: Date,
+                 val quantity: Double,
+                 val price: Double,
+                 val total: Double,
+                 val fillType: FillType,
+                 val orderType: OrderType)
+
+enum class FillType {
+    FILL,
+    PARTIAL_FILL
+}
+
+enum class OrderType {
+    BUY,
+    SELL
+}

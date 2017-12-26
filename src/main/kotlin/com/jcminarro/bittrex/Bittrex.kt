9e3c69df20e6e97c40cd4801bac998d2f1f0fc0f
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-12-26 13:18:17 +0100

    Create Bittrex client

---
+++ src/main/kotlin/com/jcminarro/bittrex/Bittrex.kt
@@ -0,0 +1,29 @@
+package com.jcminarro.bittrex
+
+import com.jcminarro.bittrex.api.PublicAPI
+
+class Bittrex {
+
+    private val publicApi by lazy { PublicAPI() }
+
+    @Throws(BittrexException::class)
+    fun getMarkets() = publicApi.getMarkets()
+
+    @Throws(BittrexException::class)
+    fun getCurrencies() = publicApi.getCurrencies()
+
+    @Throws(BittrexException::class)
+    fun getTick(market: String) = publicApi.getTick(market)
+
+    @Throws(BittrexException::class)
+    fun getMarketSummaries() = publicApi.getMarketSummaries()
+
+    @Throws(BittrexException::class)
+    fun getMarketSummary(market: String) = publicApi.getMarketSummary(market)                   
+
+    @Throws(BittrexException::class)
+    fun getOrderbook(market: String, vararg orderType: OrderType) = publicApi.getOrderbook(market, arrayOf(*orderType))
+
+    @Throws(BittrexException::class)
+    fun getMarketHistory(market: String) = publicApi.getMarketHistory(market)
+}

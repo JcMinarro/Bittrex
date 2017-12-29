ef509ae31587319f4888beb99519d3bfc9acd5cd
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-12-29 16:23:22 +0100

    Add access to Account calls from Bittrex client

--- src/main/kotlin/com/jcminarro/bittrex/Bittrex.kt
+++ src/main/kotlin/com/jcminarro/bittrex/Bittrex.kt
@@ -1,10 +1,12 @@
 package com.jcminarro.bittrex
 
+import com.jcminarro.bittrex.api.accountapi.AccountAPI
 import com.jcminarro.bittrex.api.publicapi.PublicAPI
 
 class Bittrex(bittrexCredentials: BittrexCredentials = NON_CREDENTIAL) {
 
     private val publicApi by lazy { PublicAPI(bittrexCredentials) }
+    private val accountApi by lazy { AccountAPI(bittrexCredentials) }
 
     @Throws(BittrexException::class)
     fun getMarkets() = publicApi.getMarkets()
@@ -26,4 +28,29 @@ class Bittrex(bittrexCredentials: BittrexCredentials = NON_CREDENTIAL) {
 
     @Throws(BittrexException::class)
     fun getMarketHistory(market: String) = publicApi.getMarketHistory(market)
+
+    @Throws(BittrexException::class)
+    fun getBalances() = accountApi.getBalances()
+
+    @Throws(BittrexException::class)
+    fun getBalance(currency: String) = accountApi.getBalance(currency)
+
+    @Throws(BittrexException::class)
+    fun getDepositAddress(currency: String) = accountApi.getDepositAddress(currency)
+
+    @Throws(BittrexException::class)
+    fun getOrderHistory(market:String? = null) = accountApi.getOrderHistory(market)
+
+    @Throws(BittrexException::class)
+    fun getOrder(uuid: String) = accountApi.getOrder(uuid)
+
+    @Throws(BittrexException::class)
+    fun getWithdrawalHistory() = accountApi.getWithdrawalHistotry()
+
+    @Throws(BittrexException::class)
+    fun getDepositHistory() = accountApi.getDepositHistory()
+
+    @Throws(BittrexException::class)
+    fun withdraw(currency: String, quantity: Double, address: String, paymentId: String? = null): WithdrawalRequested =
+            accountApi.withdraw(currency, quantity, address, paymentId)
 }

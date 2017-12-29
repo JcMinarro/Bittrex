3e55bb42a5af4c0e5f65d81cb56fe5f586895307
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-12-29 13:09:31 +0100

    Add Account Models

--- src/main/kotlin/com/jcminarro/bittrex/Models.kt
+++ src/main/kotlin/com/jcminarro/bittrex/Models.kt
@@ -3,6 +3,10 @@ package com.jcminarro.bittrex
 import java.util.*
 
 typealias MarketHistory = List<Trade>
+typealias BalanceHistory = List<Balance>
+typealias WithdrawalHistory = List<Withdrawal>
+typealias DepositHistory = List<Deposit>
+typealias OrderHistory = List<Order>
 
 data class Market(val marketCurrencySymbol: String,
                   val marketCurrencyName: String,
@@ -51,6 +55,62 @@ data class Trade(val id: Int,
                  val total: Double,
                  val fillType: FillType,
                  val orderType: OrderType)
+
+data class Balance(
+        val currency: String,
+        val balance: Double,
+        val available: Double,
+        val pending: Double,
+        val cryptoAddress: String,
+        val requested: Boolean,
+        val uuid: String)
+
+data class DepositAddress(
+        val currency: String,
+        val address: String
+)
+
+data class WithdrawalRequested(val uuid: String)
+
+data class Order(
+        val orderUuid: String,
+        val exchange: String,
+        val orderType: OrderType,
+        val quantity: Double,
+        val quantityRemaining: Double,
+        val limit: Double,
+        val commissionPaid: Double,
+        val price: Double,
+        val pricePerUnit: Double,
+        val opened: Date,
+        val closed: Date?,
+        val cancelInitiated: Boolean,
+        val immediateOrCancel: Boolean,
+        val isConditional: Boolean,
+        val condition: String?,
+        val conditionTarget: String)
+
+data class Withdrawal(
+        val paymentUuid: String,
+        val currency: String,
+        val amount: Double,
+        val address: String,
+        val opened: Date,
+        val authorized: Boolean,
+        val pendingPayment: Boolean,
+        val txCost: Double,
+        val txId: String,
+        val canceled: Boolean,
+        val invalidAddress: Boolean)
+
+data class Deposit(
+        val id: Int,
+        val amount: Double,
+        val currency: String,
+        val confirmations: Int,
+        val lastUpdated: Date,
+        val txId: String,
+        val cryptoAddress: String)
 
 data class BittrexCredentials(val key: String,
                               val secret: String) {

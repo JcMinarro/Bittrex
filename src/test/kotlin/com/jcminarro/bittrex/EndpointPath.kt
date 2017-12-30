daa7f7e56cc19097b458c1b7115b281d3db2788f
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-12-30 15:32:46 +0100

    Add Endpoint Path for Account calls for test proposes

--- src/test/kotlin/com/jcminarro/bittrex/EndpointPath.kt
+++ src/test/kotlin/com/jcminarro/bittrex/EndpointPath.kt
@@ -12,4 +12,13 @@ object EndpointPath {
     val GET_MARKET_SUMMARY = WireMock.urlPathMatching("/public/getmarketsummary")
     val GET_ORDERBOOK = WireMock.urlPathMatching("/public/getorderbook")
     val GET_MARKET_HISOTRY = WireMock.urlPathMatching("/public/getmarkethistory")
+
+    val GET_BALANCES = WireMock.urlPathMatching("/account/getbalances")
+    val GET_BALANCE = WireMock.urlPathMatching("/account/getbalance")
+    val WITHDRAW = WireMock.urlPathMatching("/account/withdraw")
+    val GET_DEPOSIT_ADDRESS = WireMock.urlPathMatching("/account/getdepositaddress")
+    val GET_ORDER = WireMock.urlPathMatching("/account/getorder")
+    val GET_ORDER_HISTORY = WireMock.urlPathMatching("/account/getorderhistory")
+    val GET_WITHDRAWAL_HISTORY = WireMock.urlPathMatching("/account/getwithdrawalhistory")
+    val GET_DEPOSIT_HISTORY = WireMock.urlPathMatching("/account/getdeposithistory")
 }
